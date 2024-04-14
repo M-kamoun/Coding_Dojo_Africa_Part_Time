bbefe084ce3713ca035d1dd4496ea71924fc859e
Kamoun Mehdi <kammahdouch@gmail.com>
2024-04-14 16:52:19 +0100

    add core assignement JS button clicked

---
+++ Web_fundamentals/JAVASCRIPT/Core/Button clicker/script.js
@@ -0,0 +1,13 @@
+function change(elem) {
+  if (elem.innerText == "LogOut") {
+    elem.innerText = "LogIn";
+  } else elem.innerText = "LogOut";
+}
+
+function message() {
+  alert("Ninja was liked!");
+}
+
+function hide(elem) {
+  elem.remove();
+}
