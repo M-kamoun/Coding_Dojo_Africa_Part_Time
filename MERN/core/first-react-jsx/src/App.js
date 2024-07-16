04bf0a960ef2cea6f3dc9eab22897ef9d31c5d5b
Kamoun Mehdi <kammahdouch@gmail.com>
2024-07-16 13:49:24 +0100

    Core :  OUR FIRST REACT JSX

---
+++ MERN/core/first-react-jsx/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello DOJO!</h1>
+      <p>Things i need to do : </p>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt EVEREST</li>
+        <li>Run a Marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
