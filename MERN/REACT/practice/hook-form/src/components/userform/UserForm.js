f641461afce69686b39a15998b31196f95e3ca45
Kamoun Mehdi <kammahdouch@gmail.com>
2024-07-22 12:58:17 +0100

    core Hook Form with validations

--- MERN/REACT/practice/hook-form/src/components/userform/UserForm.js
+++ MERN/REACT/practice/hook-form/src/components/userform/UserForm.js
@@ -2,67 +2,119 @@ import React, { useState } from "react";
 import Style from "./Userform.module.css";
 
 const UserForm = (props) => {
-  const [user, setFirstname] = useState({
+  const initValues = {
     firstname: "",
     lastname: "",
     email: "",
     password: "",
     confirmpassword: "",
-  });
+  };
+  const initError = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  };
+  const [user, setUser] = useState(initValues);
+  const [error, setError] = useState(initError);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    let errorMessage = "";
+
+    switch (name) {
+      case "firstname":
+        errorMessage =
+          value.length < 2 ? "first Name must be at least 2 characters!" : "";
+        break;
+      case "lastname":
+        errorMessage =
+          value.length < 2 ? "first Name must be at least 2 characters!" : "";
+        break;
+      case "email":
+        errorMessage =
+          value.length < 2 ? "first Name must be at least 2 characters!" : "";
+        break;
+      case "password":
+        errorMessage =
+          value.length < 8 ? "first Name must be at least 8 characters!" : "";
+        break;
+      case "confirmpassword":
+        errorMessage = value !== user.password ? "Password must match!" : "";
+        break;
+      default:
+        break;
+    }
+
+    setError({ ...error, [name]: errorMessage });
+    setUser({ ...user, [name]: value });
+  };
 
   return (
     <>
-      <form>
-        <div>
-          <div className={Style.formControl}>
-            <label>First Name: </label>
-            <input
-              type="text"
-              value={user.firstname}
-              onChange={(e) =>
-                setFirstname({ ...user, firstname: e.target.value })
-              }
-            />
-          </div>
-          <div className={Style.formControl}>
-            <label>Last Name: </label>
-            <input
-              type="text"
-              value={user.lastname}
-              onChange={(e) =>
-                setFirstname({ ...user, lastname: e.target.value })
-              }
-            />
-          </div>
-          <div className={Style.formControl}>
-            <label>email: </label>
-            <input
-              type="text"
-              value={user.email}
-              onChange={(e) => setFirstname({ ...user, email: e.target.value })}
-            />
-          </div>
-          <div className={Style.formControl}>
-            <label>password: </label>
-            <input
-              type="password"
-              value={user.password}
-              onChange={(e) =>
-                setFirstname({ ...user, password: e.target.value })
-              }
-            />
-          </div>
-          <div className={Style.formControl}>
-            {" "}
-            <label>confirm password: </label>
-            <input
-              type="password"
-              value={user.confirmpassword}
-              onChange={(e) =>
-                setFirstname({ ...user, confirmpassword: e.target.value })
-              }
-            />
-          </div>
+      <pre>Error : {JSON.stringify(error)}</pre>
+      <pre>User : {JSON.stringify(user)}</pre>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className={Style.formControl}>
+          <label>First Name: </label>
+          <input
+            type="text"
+            name="firstname"
+            value={user.firstname}
+            onChange={handleChange}
+          />
+          {error.firstname && (
+            <div className={Style.error}>{error.firstname}</div>
+          )}
+        </div>
+
+        <div className={Style.formControl}>
+          <label>Last Name: </label>
+          <input
+            type="text"
+            value={user.lastname}
+            name="lastname"
+            onChange={handleChange}
+          />
+          {error.lastname && (
+            <div className={Style.error}>{error.lastname}</div>
+          )}
+        </div>
+        <div className={Style.formControl}>
+          <label>email: </label>
+          <input
+            type="text"
+            value={user.email}
+            name="email"
+            onChange={handleChange}
+          />
+          {error.email && <div className={Style.error}>{error.email}</div>}
+        </div>
+        <div className={Style.formControl}>
+          <label>password: </label>
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          {error.password && (
+            <div className={Style.error}>{error.password}</div>
+          )}
+        </div>
+        <div className={Style.formControl}>
+          <label>confirm password: </label>
+          <input
+            type="password"
+            name="confirmpassword"
+            value={user.confirmpassword}
+            onChange={handleChange}
+          />
+          {error.confirmpassword && (
+            <div className={Style.error}>{error.confirmpassword}</div>
+          )}
         </div>
       </form>
       <div className={Style.display}>
